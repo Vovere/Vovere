9a223a8c890452d05215088bfaa0c2ab63a7993d
Vovere <laurad.009@gmail.com>
2016-06-04 13:31:50 +0300

    Simple changes
    Simple changes

---
+++ script.js
@@ -0,0 +1,12 @@
+var surname = prompt('Greetings friend, may I enquire as to your surname?');
+var surname;
+var age;
+name = "Andy";
+age = 43;
+
+
+var i = 1;
+while (i < 10) {
+    alert(i);
+    i = i + 1;
+}
